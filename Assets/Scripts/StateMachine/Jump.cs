33f952fe34eb251610f30aa264aee0cf48a68f25
Tyreyn <athosikish33@gmail.com>
2023-02-24 18:13:36 +0100

    add fastjump handling

--- Assets/Scripts/StateMachine/Jump.cs
+++ Assets/Scripts/StateMachine/Jump.cs
@@ -4,12 +4,14 @@ public class Jump : State
 {
     private bool m_Jumping = true;
     private bool m_Jumped = false;
+    private Vector3 mapOldVector3;
     public Jump(GameObject Player, StateMachine.StateMachine StateMachine) : base(Player, StateMachine)
     {
         canMove = false;
     }
     public override void OnEntry()
     {
+        mapOldVector3 = Vector3.zero;
         m_Jumping = true;
         m_Jumped = false;
         Player.GetComponent<Player>().Animator.Play(this.ToString());
@@ -18,8 +20,9 @@ public class Jump : State
     {
         if (m_Jumping)
         {
-            m_Jumping = false;
+            this.m_Jumping = false;
             this.PlayerScript.isGround = false;
+            this.mapOldVector3 = new Vector3(this.PlayerScript.SpeedX, 0, this.PlayerScript.SpeedZ);
             this.Rigidbody.AddForce(new Vector3(this.PlayerScript.SpeedX, 1.5f, this.PlayerScript.SpeedZ) * this.PlayerScript.Acc, ForceMode.Impulse);
         }
 
@@ -38,10 +41,16 @@ public class Jump : State
     {
         if (this.PlayerScript.ActionQueue.Count != 0)
         {
-            if (this.PlayerScript.ActionQueue.Peek() == CharacterAction.Jump)
+            if (this.PlayerScript.ActionQueue.Peek() == CharacterAction.Jump && this.PlayerScript.SpeedX != 0 ||
+                this.PlayerScript.ActionQueue.Peek() == CharacterAction.Jump && this.PlayerScript.SpeedZ != 0)
             {
                 Debug.print("Lecymy do " + StateMachine.FastJump.ToString());
                 StateMachine.ChangeState(StateMachine.FastJump);
+            }
+            else
+            {
+                Debug.print("wychodze");
+                StateMachine.ChangeState(StateMachine.Idle);
             }
         }
         else
